27e42c48d9d477c22824a984a1bdda7840cb0d98
Waqas <130844309+adamsmith19899@users.noreply.github.com>
2024-12-31 18:05:04 +0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ const Header = () => {
             <a href="#features" className="nav-link">Features</a>
             <a href="#how-it-works" className="nav-link">How it Works</a>
             <a href="#faqs" className="nav-link">FAQs</a>
-            <a href="#features" className="download-btn">
+            <a href="#features" button className="download-btn">
               <Download className="h-4 w-4 mr-2" />
               Download
             </button>
