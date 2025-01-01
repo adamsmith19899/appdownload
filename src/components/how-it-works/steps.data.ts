87fb857fb40cbde92c66d048ddd7a2b45b7576a4
Waqas <130844309+adamsmith19899@users.noreply.github.com>
2025-01-02 01:44:49 +0500

    Update steps.data.ts

--- src/components/how-it-works/steps.data.ts
+++ src/components/how-it-works/steps.data.ts
@@ -5,7 +5,7 @@ export const steps = [
 icon: Download,
 title: "Download & Install",
 description: "Get started in seconds by downloading Capcut Apk. Run the installation package and follow the on-screen instructions.",
-image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiZ2sSdq7SgKL6FX26RSHNmC7AJG9_ddtcALe39hdCiqV6DnghU6uVhw3kZWQeiObx2buAO9PxlHTnm4C18Pv0dBV5oLbi-8i-cV3wKcBh7zdcHS8oyDmEGCetVsk-gu0x5yDTLHGajs6auFDki7CZHz6Vn51qUBrPG2074qp9VG5st3u7q82V9ETKRkM61/s588/sdadsadsadad.webp"
+image: "https://images.unsplash.com/photo-1551650975-87deedd944c3?auto=format&fit=crop&w=1000&q=80"
 },
 {
 icon: Settings,
@@ -17,6 +17,6 @@ image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format
 icon: Share2,
 title: "Share & Connect",
 description: "Export your edited video in various formats and share it on social media, messaging apps, or save it to your device's gallery.",
-image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiwwVlJGDDpRjs4IvDjTPYAUfdQbmlgAJy3eDzRY-ZOfnEdxzrRzEPMJfUx-u8IFYsQ5p6yNIN-h8ZkgeQ10ZdzYu7w1BChN6keodvmelnSE-5wfLSkSI5DIbcUly30RQqSP9NbDIRLaCBBbRV4gA2JedmV41sZl5Q9Sx6gpb0lbSVRQ2nSkrDc2BvKDgvv/s320/2024-11-Blog-Is-Capcut-safe-for-kids__Thumbnail.jpg"
+image: "https://images.unsplash.com/photo-1522071820081-009f0129c71c?auto=format&fit=crop&w=1000&q=80"
 }
 ];
